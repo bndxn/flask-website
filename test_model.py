cf90b1d2026e6702233a633d0fef841023058b2e
bendixonbbc <123097722+bendixonbbc@users.noreply.github.com>
2023-09-14 14:33:02 +0100

    Revert "Merge branch 'tf-lite-conversion'"
    This reverts commit 13048b8310b891be110f1d56ee32116f08869c6a, reversing
    changes made to f63a773aa944387dace8ad79b01ac64f5ed1897d.

--- test_model.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, request, jsonify
-import numpy as np
-import tflite_runtime.interpreter as tflite
-
-app = Flask(__name__)
-
-# Load TFLite model and allocate tensors
-interpreter = tflite.Interpreter(model_path="static/converted_model_fused.tflite")
-interpreter.allocate_tensors()
